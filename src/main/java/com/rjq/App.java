fe965e83a6f6a3caa3bf50abefd94609f40670e9
rjq <1695479524@qq.com>
2022-08-28 17:48:51 +0800

    rjq tijiao1

--- src/main/java/com/rjq/App.java
+++ src/main/java/com/rjq/App.java
@@ -7,5 +7,7 @@ public class App {
         System.out.println("hello,gitee");
         System.out.println("hello,git4");
         System.out.println("rjq,git4");
+        System.out.println("renjiaqing1");
+
     }
 }
